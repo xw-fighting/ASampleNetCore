6cc58d486db298e0e6238c05ab1545d4669935fd
xiaowei5966 <xiaowei5966@outlook.com>
2019-05-29 17:15:55 +0800

    mongo db test

--- src/Applications/ASample.NetCore.WebApi/Controllers/MongoDbController.cs
+++ src/Applications/ASample.NetCore.WebApi/Controllers/MongoDbController.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASample.NetCore.MongoDb;
 using ASample.NetCore.MongoDb.Model;
 using ASample.NetCore.MongoDb.Repository;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASample.NetCore.WebApi.Controllers
@@ -26,12 +26,51 @@ namespace ASample.NetCore.WebApi.Controllers
             var user = new User
             {
                 Id = Guid.NewGuid(),
-                Name = "test",
+                Name = "test2",
                 Address = "云南昆明",
                 Age = 21,
                 CreateTime = DateTime.Now
             };
-            await _repository.AddAsync(user);
+            await _repository.AddAsync(user.BindId(c => c.Id));
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<User> Get([FromRoute] User query)
+        {
+
+            var guid = query.Id;//.ToCSUUid();
+            //var luuid = new Guid("a2ed42eb-158f-0a41-ba87-a9ac1de2f4a5");
+            //var bytes = GuidConverter.ToBytes(luuid, GuidRepresentation.PythonLegacy);
+            //var csuuid = new Guid(bytes);
+            var discount = await _repository.GetAsync(c => c.Id == guid);
+
+            if (discount is null)
+            {
+                return null;
+            }
+
+            return discount;
+        }
+
+        [HttpPut]
+        public async Task UpdateAsync([FromBody]User param)
+        {
+            var user = new User
+            {
+                Id = param.Id,
+                Name = "test22",
+                Address = "云南昆明市",
+                Age = 22,
+                CreateTime = DateTime.Now
+            };
+            await _repository.UpdateAsync(user);
+        }
+
+        [HttpGet]
+        public async Task<List<User>> SelectAsync()
+        {
+            var result = await _repository.SelectAsync(c => true);
+            return result.ToList();
         }
     }
 }
--- src/Infrastrature/ASample.NetCore.MongoDb/Extension.cs
+++ src/Infrastrature/ASample.NetCore.MongoDb/Extension.cs
@@ -4,7 +4,12 @@ using ASample.NetCore.MongoDb.Model;
 using ASample.NetCore.MongoDb.Repository;
 using Autofac;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ASample.NetCore.MongoDb
 {
@@ -49,5 +54,45 @@ namespace ASample.NetCore.MongoDb
             => builder.Register(ctx => new MongoRepository<TEntity>(ctx.Resolve<IMongoDatabase>(), collectionName))
                 .As<IMongoRepository<TEntity>>()
                 .InstancePerLifetimeScope();
+
+
+        public static T Bind<T>(this T model, Expression<Func<T, object>> expression, object value)
+            => model.Bind<T, object>(expression, value);
+
+        public static T BindId<T>(this T model, Expression<Func<T, Guid>> expression)
+            => model.Bind<T, Guid>(expression, Guid.NewGuid());
+
+        private static TModel Bind<TModel, TProperty>(this TModel model, Expression<Func<TModel, TProperty>> expression,
+            object value)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                memberExpression = ((UnaryExpression)expression.Body).Operand as MemberExpression;
+            }
+
+            var propertyName = memberExpression.Member.Name.ToLowerInvariant();
+            var modelType = model.GetType();
+            var field = modelType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .SingleOrDefault(x => x.Name.ToLowerInvariant().StartsWith($"<{propertyName}>"));
+            if (field == null)
+            {
+                return model;
+            }
+
+            field.SetValue(model, value);
+
+            return model;
+        }
+
+
+        public static Guid ToCSUUid(this Guid guid)
+        {
+            BsonDefaults.GuidRepresentation = GuidRepresentation.PythonLegacy;
+            var luuid = new Guid(guid.ToString());
+            var bytes = GuidConverter.ToBytes(luuid, GuidRepresentation.PythonLegacy);
+            var csuuid = new Guid(bytes);
+            return csuuid;
+        }
     }
 }
